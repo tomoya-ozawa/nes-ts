ef704defb0e9bdaadc87d87e810e741e7c9c44b3
t.ozawa <t.ozawa1230@gmail.com>
2024-06-21 01:21:24 +0900

    fix render process

--- src/app/nes/ppu.ts
+++ src/app/nes/ppu.ts
@@ -85,37 +85,33 @@ export default class PPU {
 
   // TODO: 属性テーブルやpaletteを反映させる
   public render() {
-    const display = new Uint8Array(256 * 240 * 4); // 256x240 ピクセル、各ピクセル4チャネル (RGBA)
+    // 256x240 ピクセル、各ピクセル4チャネル (RGBA)
+    const display = new Uint8Array(256 * 240 * 4);
+    let displayIndex = 0;
 
-    for (let nameIndex = 0x2000; nameIndex < 0x23bf; nameIndex++) {
-      const target = this.vram.get(nameIndex).toNumber();
-      if (target === 0) {
-        continue;
-      }
+    // 描画するscanline
+    for (let y = 0; y < 239; y++) {
+      // 描画するpixel. 1タイル = 8pixelなので、8ごと増加する
+      for (let x = 0; x < 255; x = x + 8) {
+        // xとyから、対応するネームテーブルのindexを割り出す
+        const nameIndex = (y >> 3) * 32 + (x >> 3) + 0x2000;
+        const target = this.vram.get(nameIndex).toNumber();
 
-      // 描画するx,yのUint8Arrayの開始位置を割り出す
-      const nameI = nameIndex - 0x2000;
-      const column = Math.floor(nameI / 32);
-      const yOffset = column * 256 * 8;
-      const xOffset = (nameI % 32) * 8;
-      const arrayIndexOffset = (yOffset + xOffset) * 4;
-
-      // スプライトの描画
-      for (let i = 0; i < 8; i++) {
-        const byte1 = this.chrom[target * 16 + i];
-        const byte2 = this.chrom[target * 16 + i + 8];
+        // スプライトの描画
+        const chromAddress = target * 16 + (y % 8);
+        const byte1 = this.chrom[chromAddress];
+        const byte2 = this.chrom[chromAddress + 8];
 
         for (let bit = 0; bit < 8; bit++) {
           const bit1 = (byte1 >> (7 - bit)) & 1;
           const bit2 = (byte2 >> (7 - bit)) & 1;
           // グレースケールの色値
           const color = (bit1 + (bit2 << 1)) * 85;
-          // 描画するx,yの位置 + iバイトごとに行(256)分加算
-          const offset = arrayIndexOffset + (i * 256 + bit) * 4;
-          display[offset] = color;
-          display[offset + 1] = color;
-          display[offset + 2] = color;
-          display[offset + 3] = 1;
+          displayIndex = displayIndex + 4;
+          display[displayIndex] = color;
+          display[displayIndex + 1] = color;
+          display[displayIndex + 2] = color;
+          display[displayIndex + 3] = 1;
         }
       }
     }
